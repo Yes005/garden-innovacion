99e9397a31ee6e7017027075a03392e2f119099f
yes <yeshuab3@gmail.com>
2025-02-28 15:55:55 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+        <h3 style={{textAlign: 'center', padding: '2rem 0'}}>Manuales creados por el Equipo I+D+i en diferentes temas enfocados en la computación en la nube</h3> 
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/category/gcp-garden">
+            Ingresar
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  );
+}
